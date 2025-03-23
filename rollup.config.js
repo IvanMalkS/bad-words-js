72671c2e3467ee57b19a9d636267d79803100c0a
Van1s1mys <malkovsin@gmail.com>
2025-03-23 11:51:56 +0500

    v1.1.7 Поправил экспорты и перевёл на umd

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
 import terser from '@rollup/plugin-terser';
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
- 
+
 export default {
     input: "src/index.ts",
     output: [
         { dir: "dist/cjs", format: "cjs" },
-        { dir: "dist/min", format: "cjs", plugins:      [terser()] },
-        { dir: "dist", format: "esm" },
+        { dir: "dist/esm", format: "esm" },
+        { file: "dist/index.umd.js", format: "umd", name: "RussianBadWordCensor" },
+        { file: "dist/index.umd.min.js", format: "umd", name: "RussianBadWordCensor", plugins: [terser()] },
     ],
     plugins: [
         typescript({ useTsconfigDeclarationDir: true, tsconfig: "tsconfig.json" }),
